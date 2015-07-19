eadde1a537eef4d968628d1d838c07fb1953af52
Adil Moujahid <adil.mouja@gmail.com>
2015-07-19 17:27:02 +0900

    Changed Pymongo Connection to Pymongo MongoClient

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import render_template
-from pymongo import Connection
+from pymongo import MongoClient
 import json
 from bson import json_util
 from bson.json_util import dumps
@@ -21,15 +21,15 @@ def index():
 
 @app.route("/donorschoose/projects")
 def donorschoose_projects():
-    connection = Connection(MONGODB_HOST, MONGODB_PORT)
+    connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
     collection = connection[DBS_NAME][COLLECTION_NAME]
-    projects = collection.find(fields=FIELDS, limit=50000)
-    #projects = collection.find(fields=FIELDS)
+    projects = collection.find(projection=FIELDS, limit=100000)
+    #projects = collection.find(projection=FIELDS)
     json_projects = []
     for project in projects:
         json_projects.append(project)
     json_projects = json.dumps(json_projects, default=json_util.default)
-    connection.disconnect()
+    connection.close()
     return json_projects
 
 if __name__ == "__main__":
